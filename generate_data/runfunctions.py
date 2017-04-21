ff9ff9558dcaeb7035d20579fba62774a05b30a2
Dan Tamayo <tamayo.daniel@gmail.com>
2017-04-21 01:07:42 -0400

    whfastias15comparison

--- generate_data/runfunctions.py
+++ generate_data/runfunctions.py
@@ -9,20 +9,16 @@ def collision(reb_sim, col):
     reb_sim.contents._status = 5 # causes simulation to stop running and have flag for whether sim stopped due to collision
     return 0
 
-def run_system(sim_id, shadow):
-    maxorbs = 1.e9
-    datapath = '../../data/random/'
+def run_random(sim_id, integrator="whfast", dt=0.09, maxorbs=1.e9, betamin=1., betamax=30., shadow=False, runstr=None):
+    datapath = '../data/random/'
 
     a1 = 1. # All distances in units of the innermost semimajor axis (always at 1)
     Mstar = 1. # All masses in units of stellar mass
 
     logMmin = np.log10(1.e-7) # 1/3 Mars around Sun
     logMmax = np.log10(1.e-4) # 2 Nep around Sun
-    logemin = np.log10(1.e-3)
     logincmin = np.log10(1.e-3)
     logincmax = np.log10(1.e-1) # max mutual inclination of 11.4 degrees. Absolute of 5.7 deg
-    betamin = 1. # min separation in Hill radii
-    betamax = 5.
 
     seed(sim_id)
 
@@ -33,32 +29,43 @@ def run_system(sim_id, shadow):
     hill12 = a1*((M1+M2)/3.)**(1./3.)
     beta1 = uniform(betamin, betamax)
     a2 = a1 + beta1*hill12
-    ecrit12 = (a2-a1)/a2
 
     hill23 = a2*((M2+M3)/3.)**(1./3.)
     beta2 = uniform(betamin, betamax)
     a3 = a2 + beta2*hill23
-    ecrit23 = (a3-a2)/a3
 
     minhill = min(hill12, hill23)
 
-    logemax1 = np.log10(ecrit12)
-    logemax2 = np.log10(min(ecrit12, ecrit23))
-    logemax3 = np.log10(ecrit23)
+    ecrit1 = (a2-a1)/a1
+    ecrit21 = (a2-a1)/a2
+    ecrit23 = (a3-a2)/a2
+    ecrit3 = (a3-a2)/a3
+    
+    logemax1 = np.log10(ecrit1)
+    logemax2 = np.log10(min(ecrit21, ecrit23))
+    logemax3 = np.log10(ecrit3)
 
+    logemin1 = np.log10(M2/ecrit1**2)
+    logemin2 = np.log10(max(M1/ecrit21**2, M3/ecrit23**2))
+    logemin3 = np.log10(M2/ecrit3**2)
+
+    #print("M1 = {0}, M2 = {1}, M3 = {2}".format(M1, M2, M3))
     #print("beta1 = {0}, beta2 = {1}".format(beta1, beta2))
+    #print("a1 = {0}, a2 = {1}, a3 = {2}".format(a1, a2, a3))
     #print("emax1 = {0}, emax2 = {1}, emax3 = {2}".format(10**logemax1, 10**logemax2, 10**logemax3))
+    #print("emin1 = {0}, emin2 = {1}, emin3 = {2}".format(10**logemin1, 10**logemin2, 10**logemin3))
+    #print("minhill = {0}".format(minhill))
 
-    e1 = min(10.**uniform(logemin, logemax1), 1.) # make sure ecc < 1
-    e2 = min(10.**uniform(logemin, logemax2), 1.)
-    e3 = min(10.**uniform(logemin, logemax3), 1.)
+    e1 = min(10.**uniform(logemin1, logemax1), 1.) # make sure ecc < 1
+    e2 = min(10.**uniform(logemin2, logemax2), 1.)
+    e3 = min(10.**uniform(logemin3, logemax3), 1.)
 
     i1 = 10.**uniform(logincmin, logincmax)
     i2 = 10.**uniform(logincmin, logincmax)
     i3 = 10.**uniform(logincmin, logincmax)
 
     sim = rebound.Simulation()
-    sim.integrator="whfast"
+    sim.integrator=integrator
     sim.ri_whfast.safe_mode = 0
     sim.G = 4*np.pi**2
 
@@ -73,22 +80,32 @@ def run_system(sim_id, shadow):
         kicksize=1.e-11
         ps[2].x += kicksize
 
-    sim.dt = 0.09 # 0.09 of inner orbital period
+    if integrator=="whfast":
+        sim.dt = dt*sim.particles[1].P
     sim.collision = "direct"
     sim.collision_resolve = collision
         
-    runstr = "{0:0=7d}.bin".format(sim_id)
+    #runstr = "{0:0=7d}.bin".format(sim_id)
     if shadow:
         shadowstr = 'shadow'
-        print(shadowstr)
     else:
         shadowstr = ''
 
-    sim.save(datapath+'initial_conditions/'+shadowstr+'runs/ic'+runstr)
-    sim.initSimulationArchive(datapath+'simulation_archives/'+shadowstr+'runs/sa'+runstr, interval=maxorbs/1000.)
-    sim.integrate(maxorbs) # will stop if collision occurs
-    sim.save(datapath+'final_conditions/'+shadowstr+'runs/fc'+runstr)
+    if runstr:
+        sim.save(datapath+'initial_conditions/'+shadowstr+'runs/ic'+runstr)
+        sim.initSimulationArchive(datapath+'simulation_archives/'+shadowstr+'runs/sa'+runstr, interval=maxorbs/1000.)
 
-sim_id = int(sys.argv[1])
-run_system(sim_id, 0) # run base system
-run_system(sim_id, 1) # run shadow system
+    E0 = sim.calculate_energy()
+    t0 = time.time()
+    sim.integrate(maxorbs) # will stop if collision occurs
+    Ef = sim.calculate_energy()    
+    Eerr = abs((Ef-E0)/E0)
+
+    if runstr:
+        sim.save(datapath+'final_conditions/'+shadowstr+'runs/fc'+runstr)
+
+    return (sim.t, Eerr, time.time()-t0)
+
+#sim_id = int(sys.argv[1])
+#run_system(sim_id, save=True) # run system
+#run_system(sim_id, shadow=True, save=True) # run shadow system
